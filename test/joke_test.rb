6a30e354065d7b938261559aebddfe4ffef8b145
Sejin Kim <sejinkim@Sejins-MacBook-Pro.local>
2019-05-08 09:16:34 -0600

    Finish iteration 1

---
+++ test/joke_test.rb
@@ -0,0 +1,18 @@
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/joke'
+require 'pry'
+
+class JokeTest < MiniTest::Test
+  def setup
+    @joke = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+  end
+
+  def test_joke_has_attributes
+
+    assert_equal 1, @joke.id
+    assert_equal "Why did the strawberry cross the road?", @joke.setup
+    assert_equal "Because his mother was in a jam.", @joke.punchline
+  end
+end
