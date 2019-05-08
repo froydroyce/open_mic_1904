62f161c245724b42d56c9d7ea1cba3faaad2adbf
Sejin Kim <sejinkim@Sejins-MacBook-Pro.local>
2019-05-08 09:50:49 -0600

    Finish iteration 3

--- lib/user.rb
+++ lib/user.rb
@@ -9,4 +9,15 @@ class User
   def learn(joke)
     @jokes.push(joke)
   end
+
+  def tell(name, joke)
+    name.learn(joke)
+  end
+
+  def joke_by_id(id)
+    joke_id = jokes.find_all do |joke|
+    id == joke.id
+    end
+    joke_id.first
+  end
 end
--- test/user_test.rb
+++ test/user_test.rb
@@ -8,6 +8,7 @@ require 'pry'
 class UserTest < MiniTest::Test
   def setup
     @sal = User.new("Sal")
+    @ali = User.new("Ali")
     @joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
     @joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
   end
@@ -32,5 +33,20 @@ class UserTest < MiniTest::Test
     @sal.learn(@joke_2)
 
     assert_equal [@joke_1, @joke_2], @sal.jokes
+  end
+
+  def test_ali_can_learn_jokes_by_sal_telling_jokes
+    @sal.tell(@ali, @joke_1)
+    @sal.tell(@ali, @joke_2)
+
+    assert_equal [@joke_1, @joke_2], @ali.jokes
+  end
+
+  def test_jokes_can_be_identified_with_id
+    @sal.tell(@ali, @joke_1)
+    @sal.tell(@ali, @joke_2)
+
+    assert_equal @joke_1, @ali.joke_by_id(1)
+    assert_equal @joke_2, @ali.joke_by_id(2)
   end
 end
